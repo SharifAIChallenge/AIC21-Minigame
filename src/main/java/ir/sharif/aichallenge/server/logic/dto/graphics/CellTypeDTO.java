262ff072b3f8c82ff27e37ba2b49a591ce74813c
m.dehghankar <mohsendehghankar@gmail.com>
2021-04-29 19:38:46 +0430

    cell types for bases in graphics log changed

--- src/main/java/ir/sharif/aichallenge/server/logic/dto/graphics/CellTypeDTO.java
+++ src/main/java/ir/sharif/aichallenge/server/logic/dto/graphics/CellTypeDTO.java
@@ -1,5 +1,6 @@
 package ir.sharif.aichallenge.server.logic.dto.graphics;
 
+import ir.sharif.aichallenge.server.logic.model.cell.Cell;
 import ir.sharif.aichallenge.server.logic.model.cell.CellType;
 
 public class CellTypeDTO {
@@ -8,16 +9,11 @@ public class CellTypeDTO {
     public int col;
     public int cell_type;
 
-    public CellTypeDTO(int row, int col, CellType cell_type) {
+    public CellTypeDTO(int row, int col, CellType cell_type, int colonyID) {
         this.row = row;
         this.col = col;
         if (cell_type == CellType.BASE) {
-            if (hasPassed) {
-                this.cell_type = 1;
-            } else {
-                this.cell_type = 0;
-                hasPassed = true;
-            }
+            this.cell_type = colonyID;
         } else
             this.cell_type = (cell_type == CellType.BASE) ? 0 : cell_type.getValue() + 1;
     }
